50593e25132638ec64bebaa92cebf0b398c06946
Abhishek <abhisheksinghasr734@gmail.com>
2024-01-16 19:21:09 +0530

    first class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int exp1 = 10;
+        int exp2 = 20;
+        System.out.println(exp1+ " " + exp2);
+        String a = "abcd123";
+        char c = a.charAt(3);
+        System.out.println(c);
+    }
+}
---
+++ src/takingInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class takingInput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+//        int exp1 = sc.nextInt();
+//        float f = sc.nextFloat();
+//        String s = sc.nextLine();
+//
+//        System.out.println(exp1);
+//
+//        System.out.println(f);
+
+        float b=20.4588997766f;
+        double d=20.4588997766;
+        System.out.println(b);
+        System.out.println(d);
+    }
+}
